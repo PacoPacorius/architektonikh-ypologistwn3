328ee742ade0769c3d4e6e4ed29a997c7951ded3
Flilippos Tolias <filtol001@yahoo.gr>
2022-12-18 22:29:09 +0200

    add benchmark scripts and fix typo in README

---
+++ benchmarks/l1i_size.sh
@@ -0,0 +1,45 @@
+# #!/bin/bash
+gem5="${HOME}/gem5"
+program_location="${HOME}/university/architectoniki/spec_cpu2006"
+testVariable="l1i_size"
+options="--cpu-type=MinorCPU --caches --l2cache"
+
+command="${program_location}/401.bzip2/src/specbzip -o \"${program_location}/401.bzip2/data/input.program 10\""
+benchmark="specbzip/l1i_size"
+for l1i_size in 2kB 4kB 8kB 16kB 32kB 64kB 128kB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l1i_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l1i_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/429.mcf/src/specmcf -o \"${program_location}/429.mcf/data/inp.in\""
+benchmark="specmcf/l1i_size"
+for l1i_size in 2kB 4kB 8kB 16kB 32kB 64kB 128kB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l1i_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l1i_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/456.hmmer/src/spechmmer -o \"--fixed 0 --mean 325 --num 45000 --sd 200 --seed 0 ${program_location}/456.hmmer/data/bombesin.hmm\""
+benchmark="spechmmer/l1i_size"
+for l1i_size in 2kB 4kB 8kB 16kB 32kB 64kB 128kB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l1i_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l1i_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/458.sjeng/src/specsjeng -o ${program_location}/spec_cpu2006/458.sjeng/data/test.txt"
+benchmark="specsjeng/l1i_size"
+for l1i_size in 2kB 4kB 8kB 16kB 32kB 64kB 128kB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l1i_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l1i_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/470.lbm/src/speclibm -o \"20 ${program_location}/470.lbm/data/lbm.in 0 1 ${program_location}/470.lbm/data/100_100_130_cf_a.of\""
+benchmark="speclibm/l1i_size"
+for l1i_size in 2kB 4kB 8kB 16kB 32kB 64kB 128kB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l1i_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l1i_size} -c ${command} -I 100000000 &
+done
+wait
---
+++ benchmarks/l2_size.sh
@@ -0,0 +1,45 @@
+# #!/bin/bash
+gem5="${HOME}/gem5"
+program_location="${HOME}/university/architectoniki/spec_cpu2006"
+testVariable="l2_size"
+options="--cpu-type=MinorCPU --caches --l2cache"
+
+command="${program_location}/401.bzip2/src/specbzip -o \"${program_location}/401.bzip2/data/input.program 10\""
+benchmark="specbzip/l2_size"
+for l2_size in 32kB 64kB 128kB 256kB 512kB 1MB 2MB 4MB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l2_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l2_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/429.mcf/src/specmcf -o \"${program_location}/429.mcf/data/inp.in\""
+benchmark="specmcf/l2_size"
+for l2_size in 32kB 64kB 128kB 256kB 512kB 1MB 2MB 4MB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l2_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l2_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/456.hmmer/src/spechmmer -o \"--fixed 0 --mean 325 --num 45000 --sd 200 --seed 0 ${program_location}/456.hmmer/data/bombesin.hmm\""
+benchmark="spechmmer/l2_size"
+for l2_size in 32kB 64kB 128kB 256kB 512kB 1MB 2MB 4MB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l2_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l2_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/458.sjeng/src/specsjeng -o ${program_location}/spec_cpu2006/458.sjeng/data/test.txt"
+benchmark="specsjeng/l2_size"
+for l2_size in 32kB 64kB 128kB 256kB 512kB 1MB 2MB 4MB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l2_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l2_size} -c ${command} -I 100000000 &
+done
+wait
+
+command="${program_location}/470.lbm/src/speclibm -o \"20 ${program_location}/470.lbm/data/lbm.in 0 1 ${program_location}/470.lbm/data/100_100_130_cf_a.of\""
+benchmark="speclibm/l2_size"
+for l2_size in 32kB 64kB 128kB 256kB 512kB 1MB 2MB 4MB
+do
+    ${gem5}/build/ARM/gem5.opt -d ${benchmark}/${l2_size} ${gem5}/configs/example/se.py ${options} --${testVariable} ${l2_size} -c ${command} -I 100000000 &
+done
+wait
